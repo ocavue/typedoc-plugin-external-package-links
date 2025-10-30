66b9398901c3e64038cf0f50919f9ef2c6407b7d
ocavue <ocavue@gmail.com>
2025-10-30 16:32:01 +1100

    update code

--- src/resolvers/prosemirror.ts
+++ src/resolvers/prosemirror.ts
@@ -17,18 +17,35 @@ const prosemirrorPackages = [
 
 const prosemirrorResolver: ExternalSymbolResolver = (declaration) => {
   const packageName = declaration.moduleSource
-  if (!packageName || !prosemirrorPackages.includes(packageName)) return
+  if (!packageName || !prosemirrorPackages.includes(packageName)) {
+    return
+  }
 
   const referencePath = declaration.symbolReference?.path?.reduce(
     (str, path) => str + path.navigation + path.path,
     '',
   )
-  if (!referencePath) return
+  if (!referencePath) {
+    return
+  }
+
+  // Following the logic from the ProseMirror website
+  // https://github.com/ProseMirror/website/blob/6d86b17bc5b32e123955dcb2e24139578aa9c87b/src/build/ref.js#L35-L39
+
+  const typeName = referencePath.replace(/^\./, '')
+
+  if (typeName == 'OrderedMap')
+    return 'https://github.com/marijnh/orderedmap#readme'
+  if (typeName == 'DOMNode')
+    return 'https://developer.mozilla.org/en-US/docs/Web/API/Node'
+  if (typeName == 'DOMSelection')
+    return 'https://developer.mozilla.org/en-US/docs/Web/API/Selection'
 
   return (
     'https://prosemirror.net/docs/ref/#' +
     packageName.replace(/^prosemirror-/, '') +
-    referencePath
+    '.' +
+    typeName
   )
 }
 
