8e09ea080235074fc0bc9e211963fea6c35e4679
ocavue <ocavue@gmail.com>
2025-10-30 16:39:26 +1100

    format

--- src/resolvers/prosemirror.ts
+++ src/resolvers/prosemirror.ts
@@ -29,17 +29,12 @@ const prosemirrorResolver: ExternalSymbolResolver = (declaration) => {
     return
   }
 
-
   const typeName = referencePath.replace(/^\./, '')
 
-
   // Internal types
-  if (typeName === "DecorationGroup" ) {
+  if (typeName === 'DecorationGroup') {
     return undefined
   }
-
-
-
 
   // Following the logic from the ProseMirror website
   // https://github.com/ProseMirror/website/blob/6d86b17bc5b32e123955dcb2e24139578aa9c87b/src/build/ref.js#L35-L39
